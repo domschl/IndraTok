10ee05cd6e02deb8a843049a2c577bcdb508842e
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-08-06 12:59:32 +0200

    crc32

--- IndraTok/indra_crc_crypt.h
+++ IndraTok/indra_crc_crypt.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 uint16_t iaCrc16Ccitt(const uint8_t *data_p, unsigned long length);
+uint32_t iaCrc32(const uint8_t *s, unsigned long n);
 unsigned long iaSimpleHash(unsigned char *buf, unsigned long length);
 
 void iaXxteaEncrypt(uint32_t *v, int n, uint32_t const key[]);
