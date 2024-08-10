156b1624831640483e1e1dc8660f23ab46d3feae
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-08-10 14:00:43 +0200

    temp mess

--- IndraTok/indra_atom.c
+++ IndraTok/indra_atom.c
@@ -1,57 +1,80 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include "indra_atom.h"
 
 void iaDelete(IA_T_ATOM *pAtom) {
-  if (pAtom->onHeap) {
-    if (pAtom->type == IA_ID_ARRAY) {
-      free(pAtom->dat.heap_atom->pData);
-    } else if (pAtom->type == IA_ID_ATOM) {
-      for (uint64_t i=0; i<pAtom->count; i++) {
-        if (pAtom->dat.heap_atom[i].type == IA_ID_ARRAY) {
-          free(pAtom->dat.heap_atom[i].pData);
-        } else if (pAtom->dat.heap_atom[i].type == IA_ID_ATOM) {
-          iaDelete((IA_T_ATOM *)pAtom->dat.heap_atom[i].pData);
-        }
-      }
-    } else if (pAtom->type == IA_ID_ANYPTR) {
-      for (uint64_t i=0; i<pAtom->count; i++) {
-        free(pAtom->dat.anyPtr[i]);
-      }
-    } else {
-      printf("Unexpected type %d on heap, FATAL\n", pAtom->type);
-      exit(-1);
-    }      
+  if (pAtom->type == IA_ID_PANY) {
+    free(pAtom->data.pHeap);
+  } else if (pAtom->type == IA_ID_ATOM) {
+    for (uint64_t i=0; i<pAtom->count; i++) {
+      iaDelete(&(((IA_T_ATOM *)(pAtom->data.pHeap->pData))[i]));
+    }
+    free(pAtom->data.pHeap);
+  } else {
+    if (pAtom->onHeap) {
+      free(pAtom->data.pHeap);
+    }
   }
 }
 
-
+void iaSet(IA_T_ATOM *pAtom, int type, uint64_t recsize, uint64_t count, void *pData) {
+  pAtom->onHeap = 0;
+  pAtom->type = type;
+  if (type == IA_ID_NIL) {
+    pAtom->count = 0;
+    return;
+  }
+  pAtom->count = count;
+  if (count > iaStackMax[type]) {
+    pAtom->onHeap = 1;
+    pAtom->data.pHeap = (IA_T_HEAP *)malloc(sizeof(IA_T_HEAP_HEADER)+recsize*count);
+    pAtom->data.pHeap->capacity = count;
+    pAtom->data.pHeap->recsize = recsize;
+    memcpy(pAtom->data.pHeap->pData, pData, recsize*count);
+  } else {
+    memcpy(pAtom->data.c, pData, recsize*count);
+  }
+}
 
 void iaSetChar(IA_T_ATOM *pAtom, uint8_t value) {
   pAtom->onHeap = 0;
   pAtom->type = IA_ID_CHAR;
   pAtom->count = 1;
-  pAtom->dat.c[0] = value;
+  pAtom->data.c[0] = value;
 }
 
 void iaSetWord(IA_T_ATOM *pAtom, uint16_t value) {
   pAtom->onHeap = 0;
   pAtom->type = IA_ID_CHAR;
   pAtom->count = 1;
-  pAtom->dat.w[0] = value;
+  pAtom->data.w[0] = value;
 }
 
 void iaSetInt(IA_T_ATOM *pAtom, uint32_t value) {
   pAtom->onHeap = 0;
   pAtom->type = IA_ID_INT;
   pAtom->count = 1;
-  pAtom->dat.i[0] = value;
+  pAtom->data.i[0] = value;
 }
 
 void iaSetLong(IA_T_ATOM *pAtom, uint64_t value) {
   pAtom->onHeap = 0;
   pAtom->type = IA_ID_LONG;
   pAtom->count = 1;
-  pAtom->dat.l[0] = value;
+  pAtom->data.l[0] = value;
 }
+
+void iaSetFloat(IA_T_ATOM *pAtom, float value) {
+  pAtom->onHeap = 0;
+  pAtom->type = IA_ID_FLOAT;
+  pAtom->count = 1;
+  pAtom->data.f[0] = value;
+}
+
+void iaSetDouble(IA_T_ATOM *pAtom, double value) {
+  pAtom->onHeap = 0;
+  pAtom->type = IA_ID_DOUBLE;
+  pAtom->count = 1;
+  pAtom->data.d[0] = value;
+}
+  
--- IndraTok/indra_atom.h
+++ IndraTok/indra_atom.h
@@ -5,35 +5,42 @@
 #include <stdint.h>
 
 typedef enum types {IA_ID_NIL=0, IA_ID_CHAR, IA_ID_WORD, IA_ID_INT, IA_ID_LONG, IA_ID_FLOAT,
-                    IA_DOUBLE, IA_ID_ARRAY, IA_ID_ATOM, IA_ID_ANYPTR} IA_T_TYPES;
+                    IA_ID_DOUBLE, IA_ID_ATOM, IA_ID_PANY} IA_T_TYPES;
 
-// Heap data struct
-typedef struct _it_heap_atom {
-  uint64_t type;
-  uint64_t recsize;
-  uint64_t count;
+typedef struct _ia_t_heap_header {
+    uint64_t count;
+    uint64_t recsize;
+    } IA_T_HEAP_HEADER;
+
+typedef struct _ia_t_heap {
   uint64_t capacity;
+  uint64_t recsize;
   void *pData;
-} IA_T_HEAP_ATOM;
+} IA_T_HEAP;
 
 // Stack preference: the higher the number, the more data is stored on the stack
-// before using heap allocation. Default 1
-#define STACK_PREFERENCE 1
+// before using heap allocation. Default 1, can be overridden by defining
+#ifndef IA_STACK_PREFERENCE
+#define IA_STACK_PREFERENCE 1
+#endif
+
+typedef struct _ia_atom IA_T_ATOM;
+
+#define IA_T_ATOM_SIZE (sizeof(unsigned long)*2)
 
 // This defines the maximum array count of elements that are stored on the stack
-// If the count is greater than this, the data is stored on the heap using the IA_HEAP_ATOM struct
-#define IA_HEAP_ATOM_SIZE (sizeof(IA_T_HEAP_ATOM)*STACK_PREFERENCE)
-#define IA_STACK_DOUBLES (IA_HEAP_ATOM_SIZE/sizeof(double)*STACK_PREFERENCE)
-#define IA_STACK_FLOATS (IA_HEAP_ATOM_SIZE/sizeof(float)*STACK_PREFERENCE)
-#define IA_STACK_LONGS (IA_HEAP_ATOM_SIZE/sizeof(uint64_t)*STACK_PREFERENCE)
-#define IA_STACK_INTS (IA_HEAP_ATOM_SIZE/sizeof(uint32_t)*STACK_PREFERENCE)
-#define IA_STACK_WORDS (IA_HEAP_ATOM_SIZE/sizeof(uint16_t)*STACK_PREFERENCE)
-#define IA_STACK_CHARS (IA_HEAP_ATOM_SIZE/sizeof(uint8_t)*STACK_PREFERENCE)
-#define IA_STACK_ATOMS (IA_HEAP_ATOM_SIZE/sizeof(IA_T_HEAP_ATOM)*STACK_PREFERENCE)
-#define IA_STACK_ANYPTR (IA_HEAP_ATOM_SIZE/sizeof(void *)*STACK_PREFERENCE)
+// If the count is greater than this, the data is stored on the heap using the IA_SUBATOM struct
+#define IA_STACK_CHARS  (sizeof(uint8_t)*IA_STACK_PREFERENCE/sizeof(void *))
+#define IA_STACK_WORDS  (sizeof(uint16_t)*IA_STACK_PREFERENCE/sizeof(void *))
+#define IA_STACK_INTS   (sizeof(uint32_t)*IA_STACK_PREFERENCE/sizeof(void *))
+#define IA_STACK_LONGS  (sizeof(uint64_t)*IA_STACK_PREFERENCE/sizeof(void *))
+#define IA_STACK_FLOATS (sizeof(float)*IA_STACK_PREFERENCE/sizeof(void *))
+#define IA_STACK_DOUBLES (sizeof(double)*IA_STACK_PREFERENCE/sizeof(void *))
+
+const unsigned long iaStackMax[IA_ID_PANY+1] = {0, IA_STACK_CHARS, IA_STACK_WORDS, IA_STACK_INTS, IA_STACK_LONGS, IA_STACK_FLOATS, IA_STACK_DOUBLES, 0, 0};
 
 //* Std struct
-typedef struct _it_atom {
+struct _ia_atom {
   uint64_t onHeap:1;  //* is on heap
   uint64_t type:5;    //* data type
   uint64_t count: sizeof(long)-(1+5);  //* count of elements, <=IA_STACK_* on stack, >IA_STACK_* on heap
@@ -45,8 +52,8 @@ typedef struct _it_atom {
     uint64_t l[IA_STACK_LONGS];
     float f[IA_STACK_FLOATS];
     double d[IA_STACK_DOUBLES];
-    IA_T_HEAP_ATOM heap_atom[IA_STACK_ATOMS];
-    void *anyPtr[IA_STACK_ANYPTR];
-  } dat;
-} IA_T_ATOM;
+    IA_T_HEAP *pHeap;
+  } data;
+};
 
+const unsigned long iaTypesize[IA_ID_PANY+1] = {0, sizeof(uint8_t), sizeof(uint16_t), sizeof(uint32_t), sizeof(uint64_t), sizeof(float), sizeof(double), sizeof(struct _ia_atom), sizeof(void *)};
