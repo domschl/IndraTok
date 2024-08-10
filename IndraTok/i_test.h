78e960d59517c620e450ca5102fd26b0b8f48d46
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-08-10 09:50:56 +0200

    panic

--- IndraTok/i_test.h
+++ IndraTok/i_test.h
@@ -15,15 +15,20 @@ typedef struct _it_heap_atom {
   void *pData;
 } IT_HEAP_ATOM;
 
+// Stack preference: the higher the number, the more data is stored on the stack
+// before using heap allocation. Default 1
+#define STACK_PREFERENCE 1
+
 // This defines the maximum array count of elements that are stored on the stack
 // If the count is greater than this, the data is stored on the heap using the IT_HEAP_ATOM struct
-#define IT_HEAP_ATOM_SIZE (sizeof(IT_HEAP_ATOM))
-#define IT_STACK_DOUBLES (IT_HEAP_ATOM_SIZE/sizeof(double))
-#define IT_STACK_FLOATS (IT_HEAP_ATOM_SIZE/sizeof(float))
-#define IT_STACK_LONGS (IT_HEAP_ATOM_SIZE/sizeof(uint64_t))
-#define IT_STACK_INTS (IT_HEAP_ATOM_SIZE/sizeof(uint32_t))
-#define IT_STACK_WORDS (IT_HEAP_ATOM_SIZE/sizeof(uint16_t))
-#define IT_STACK_CHARS (IT_HEAP_ATOM_SIZE/sizeof(uint8_t))
+#define IT_HEAP_ATOM_SIZE (sizeof(IT_HEAP_ATOM)*STACK_PREFERENCE)
+#define IT_STACK_DOUBLES (IT_HEAP_ATOM_SIZE/sizeof(double)*STACK_PREFERENCE)
+#define IT_STACK_FLOATS (IT_HEAP_ATOM_SIZE/sizeof(float)*STACK_PREFERENCE)
+#define IT_STACK_LONGS (IT_HEAP_ATOM_SIZE/sizeof(uint64_t)*STACK_PREFERENCE)
+#define IT_STACK_INTS (IT_HEAP_ATOM_SIZE/sizeof(uint32_t)*STACK_PREFERENCE)
+#define IT_STACK_WORDS (IT_HEAP_ATOM_SIZE/sizeof(uint16_t)*STACK_PREFERENCE)
+#define IT_STACK_CHARS (IT_HEAP_ATOM_SIZE/sizeof(uint8_t)*STACK_PREFERENCE)
+#define IT_STACK_ATOMS (IT_HEAP_ATOM_SIZE/sizeof(IT_HEAP_ATOM)*STACK_PREFERENCE)
 
 //* Std struct
 typedef struct _it_atom {
@@ -38,7 +43,7 @@ typedef struct _it_atom {
     uint64_t l[IT_STACK_LONGS];
     float f[IT_STACK_FLOATS];
     double d[IT_STACK_DOUBLES];
-    IT_HEAP_ATOM heap_atom;
+    IT_HEAP_ATOM heap_atom[IT_STACK_ATOMS];
   } dat;
 } IT_ATOM;
 
