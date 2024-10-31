b68c44a66ea7efb7a84df32c43ef217401b5da04
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-10-31 14:44:54 +0100

    fix dup

--- IndraTok/indra_args.h
+++ IndraTok/indra_args.h
@@ -26,17 +26,4 @@ typedef struct {
     char *help;           /**< Help description of the argument */
 } IndraArg;
 
-/**
- * @brief Parses command-line arguments based on predefined argument definitions.
- *
- * This function processes command-line arguments provided to the program and matches them
- * against a set of predefined argument definitions. It supports string, boolean, and integer
- * argument types. If the `-h` or `--help` flag is provided, it prints the usage information.
- *
- * @param argc The number of command-line arguments.
- * @param argv The array of command-line argument strings.
- * @param argDefC The number of predefined argument definitions.
- * @param argDefs The array of predefined argument definitions.
- * @return true if the arguments were successfully parsed, false otherwise.
- */
 bool indraArgParse(int argc, char *argv[], int argDefC, IndraArg argDefs[]);
